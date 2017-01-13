8dbc44b0ad2194a1f6b2b8380af277b543413b41
danhamilt1 <danhamilt1@gmail.com>
2017-01-13 21:44:13 +0000

    Just a couple of comments and tidy pin names

--- src/main/java/com/energenie/energenie/EnergenieController.java
+++ src/main/java/com/energenie/energenie/EnergenieController.java
@@ -7,119 +7,120 @@ import org.springframework.stereotype.Component;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.RaspiPinNumberingScheme;
-import com.pi4j.io.gpio.impl.PinImpl;
-
-
 
 @Component("energenieController")
 public class EnergenieController {
 	private final GpioController gpio = GpioFactory.getInstance();
-	private GpioPinDigitalOutput pin11 = null;
-	private GpioPinDigitalOutput pin15 = null;
-	private GpioPinDigitalOutput pin16 = null;
-	private GpioPinDigitalOutput pin13 = null;
+	private GpioPinDigitalOutput pin00 = null;
+	private GpioPinDigitalOutput pin03 = null;
+	private GpioPinDigitalOutput pin04 = null;
+	private GpioPinDigitalOutput pin02 = null;
 	
-	private GpioPinDigitalOutput pin18 = null;
-	private GpioPinDigitalOutput pin22 = null;
+	private GpioPinDigitalOutput pin05 = null;
+	private GpioPinDigitalOutput pin06 = null;
 	
 	@PostConstruct
 	private void init(){
 		// Initialise controller pins
-		pin11 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
-		pin15 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03);
-		pin16 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04);
-		pin13 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
+		pin00 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
+		pin03 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03);
+		pin04 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04);
+		pin02 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
 		
 		// Set all pins off
-		pin11.low();
-		pin15.low();
-		pin16.low();
-		pin13.low();
+		pin00.low();
+		pin03.low();
+		pin04.low();
+		pin02.low();
 		
-		pin18 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05);
-		pin22 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06);
+		// Initialise modulation and Tx pins
+		pin05 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05);
+		pin06 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06);
 		
-		pin18.low();
-		pin22.low();
+		// Set pins to off
+		pin05.low();
+		pin06.low();
 		
 	}
 	
 	public void allOn(){
-		pin11.high();
-		pin15.high();
-		pin16.low();
-		pin13.high();
+		pin00.high();
+		pin03.high();
+		pin04.low();
+		pin02.high();
 	}
 	
 	public void allOff(){
-		pin11.high();
-		pin15.high();
-		pin16.low();
-		pin13.low();
+		pin00.high();
+		pin03.high();
+		pin04.low();
+		pin02.low();
 	}
 	
 	public void l1On(){
-		pin11.high();
-		pin15.high();
-		pin16.high();
-		pin13.high();
+		pin00.high();
+		pin03.high();
+		pin04.high();
+		pin02.high();
 		send();
 	}
 	
 	public void l1Off(){
-		pin11.high();
-		pin15.high();
-		pin16.high();
-		pin13.low();
+		pin00.high();
+		pin03.high();
+		pin04.high();
+		pin02.low();
 		send();
 	}
 	
 	public void l2On(){
-		pin11.low();
-		pin15.high();
-		pin16.high();
-		pin13.high();
+		pin00.low();
+		pin03.high();
+		pin04.high();
+		pin02.high();
 		send();
 	}
 	public void l2Off(){
-		pin11.low();
-		pin15.high();
-		pin16.high();
-		pin13.low();
+		pin00.low();
+		pin03.high();
+		pin04.high();
+		pin02.low();
 		send();
 	}
 	public void l3On(){
-		pin11.high();
-		pin15.low();
-		pin16.high();
-		pin13.high();
+		pin00.high();
+		pin03.low();
+		pin04.high();
+		pin02.high();
 		send();
 	}
 	public void l3Off(){
-		pin11.high();
-		pin15.low();
-		pin16.high();
-		pin13.low();
+		pin00.high();
+		pin03.low();
+		pin04.high();
+		pin02.low();
 		send();
 	}	
 	public void l4On(){
-		pin11.low();
-		pin15.low();
-		pin16.high();
-		pin13.high();
+		pin00.low();
+		pin03.low();
+		pin04.high();
+		pin02.high();
 		send();
 	}
 	public void l4Off(){
-		pin11.low();
-		pin15.high();
-		pin16.low();
-		pin13.low();
+		pin00.low();
+		pin03.high();
+		pin04.low();
+		pin02.low();
 		send();
 	}
 	
+	/**
+	 * Enable Tx pin, wait a bit for ramp up and Tx, then switch off.
+	 * This is a one way relationship so get no acks back.
+	 */
 	public void send(){
 		try {
 			Thread.sleep(100);
@@ -127,14 +128,14 @@ public class EnergenieController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		pin22.high();
+		pin06.high();
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		pin22.low();
+		pin06.low();
 	}
 	
 }
